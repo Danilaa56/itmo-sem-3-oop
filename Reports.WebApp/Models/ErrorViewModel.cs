ec147a4a08e7026b0a58d0efa85e23b1fcc8de58
Danilaa56 <danila.aronov@gmail.com>
2022-01-30 15:38:34 +0300

    refactor: remove Reports.WebApp project

--- Reports.WebApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Reports.WebApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
